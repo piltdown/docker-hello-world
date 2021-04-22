efe5c9f061d91422dbf7d00d236190c80405d23b
Rick Morrow <rmorrow@ubuntu-vm.localhost>
2021-04-21 21:10:46 -0700

    update endpoints

--- app.py
+++ app.py
@@ -9,7 +9,11 @@ def home():
 
 @app.route("/owner")
 def owner():
-    return f"Hello world from Rick Morrow /{datetime. now()}"
+    return f"Hello world from Rick Morrow."
+
+@app.route("/datetime")
+def get_current_time():
+    return f'{datetime.now()}'
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
